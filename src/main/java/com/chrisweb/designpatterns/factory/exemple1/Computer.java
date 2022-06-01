6ec84c4523b772dec121979ff6629a9f3f920824
Boudzoumou Chris <chris.boudzoumou@etu.univ-orleans.fr>
2022-06-01 02:35:50 +0200

    Factory example1 design pattern implementation

---
+++ src/main/java/com/chrisweb/designpatterns/factory/exemple1/Computer.java
@@ -0,0 +1,12 @@
+package com.chrisweb.designpatterns.factory.exemple1;
+
+public abstract class Computer {
+    public abstract String getRam();
+    public abstract String getHdd();
+    public abstract String getCpu();
+
+    @Override
+    public String toString() {
+        return " RAM ::" + getRam() + " | HDD :: " + getHdd() + " | CPU " + getCpu();
+    }
+}
